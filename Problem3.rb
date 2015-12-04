c9fc7304ce5eeba0b94506e7f9f6d0bb138aea1a
sle7 <sle7git@gmail.com>
2015-12-03 23:35:27 -0800

    Problem 3 Ruby added
    added problem 3 for ruby

---
+++ Problem3.rb
@@ -0,0 +1,21 @@
+# Created by sle7 on 12/3/2015.
+
+#
+#   Largest Prime Factor
+#
+n = 600_851_475_143
+
+def largestPrimeFactor(n)
+  cur = 2
+  largest = 0
+  while largest < n
+    if n % cur == 0
+      largest = cur
+      n /= cur
+    end
+    cur += 1
+  end
+  puts largest
+end
+
+largestPrimeFactor(n)
