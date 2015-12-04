405b4b2ded99bf468ea746c069e54317a056b92a
sle7 <sle7git@gmail.com>
2015-12-03 23:21:41 -0800

    Problem 2 Ruby
    Problem 2 solved in ruby

--- Problem2.rb
+++ Problem2.rb
@@ -3,3 +3,20 @@
 #
 #    Even Fibonacci Numbers
 #
+
+a = 1
+b = 2
+n = 0
+sum = 0
+max = 4000000
+
+while b < max do
+  if b % 2 == 0
+    sum += b
+  end
+  n = a + b
+  a = b
+  b = n
+end
+
+puts sum
